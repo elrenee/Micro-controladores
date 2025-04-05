b8c692292b0c965772c41a7a003be952f16d1a6e
elrenee <gon23365@uvg.edu.gt>
2025-04-05 10:16:39 -0600

    Subrutina de conversión ADC
    Implemente una subrutina que se ejecutaria cuando estuviera lista la conversión del ADC, incrementando el display 1 y cuando el dicho display llegara al tope, incrementaria en 1 el display 2 y regresaria a 0

--- Laboratorio 4/Laboratorio 4/main.c
+++ Laboratorio 4/Laboratorio 4/main.c
@@ -5,10 +5,10 @@ int main(void)
 	setup();
 	while (1)
 	{
-		PORTC= 0x01;
+		PORTC	|= (1<<PORTC0);
 		PORTD= contador;
-		PORTC= 0;
-		PORTC= 0x02;
+		PORTC	|=(0<< PORTC0);
+		PORTC	|=(1<< PORTC1);
 		PORTD= lista7seg[unidades];
 		PORTC =0;
 		PORTC = 0x04;
@@ -17,4 +17,4 @@ int main(void)
 	}
 }
 
-/****************************************/// NON-Interrupt subroutinesvoid setup(){	cli();	DDRD = 0XFF;	PORTD = 0X00;// PUERTO D COMO SALIDA, 0V EN CADA PIN 		DDRC = 0XFF;	PORTC = 0x00;//Puerto c COMO SALIDA, 0V EN CADA PIN		DDRB = 0x00;	PORTB = 0xFF;		PCMSK0 = (1<< PCINT0) | (1<< PCINT1);	PCICR = (1<< PCIE0); //interruptions habilitadas en el puerto B		//configuramos el Convertidor de analogo a digital. //	initadmux();	initimr0();		sei();}void initimr0(){	CLKPR = (1<<CLKPCE) ;	CLKPR = (1<<CS01)|(1<<CS00);	TCCR0A=0;	TCCR0B |= (1<<CS02);	TCNT0 = 158;}/*void initadmux(){	ADMUX=0;	ADMUX |= (1<< REFS0) | (1<<ADLAR) | (1<<MUX1) | (1<<MUX0)	ADCSRA= 0;	ADCSRA |= (1<<ADEN) |(1<<)}*//**************************************/// Interrupt routinesISR(PCINT0_vect){		botonesPC = PINB;	TIFR0	|= (1 << TOV0);	TCNT0=158;	TIMSK0= (1<<TOIE0);}ISR(TIMER0_OVF_vect){	TIMSK0=0;	uint8_t botonest0 = PINB;	if (botonesPC == botonest0)	{		uint8_t temporal = botonest0 & (0b00000001);		if(temporal != 0b00000001)		{			contador++;		}		temporal = botonest0 & (0b00000010);		if(temporal != 0b00000010)		{			contador--;		}	}}
+/****************************************/// NON-Interrupt subroutinesvoid setup(){	cli();	DDRD = 0XFF;	PORTD = 0X00;// PUERTO D COMO SALIDA, 0V EN CADA PIN 		DDRC = 0XFF;	PORTC = 0x00;//Puerto c COMO SALIDA, 0V EN CADA PIN		DDRB = 0x00;	PORTB = 0xFF;		PCMSK0 = (1<< PCINT0) | (1<< PCINT1);	PCICR = (1<< PCIE0); //interruptions habilitadas en el puerto B		//configuramos el Convertidor de analogo a digital. 	initadmux();	initimr0();		ADCSRA |= (1<<ADSC); //Iniciamos la Conversión	sei();}void initimr0(){	CLKPR = (1<<CLKPCE) ;	CLKPR = (1<<CS01)|(1<<CS00);	TCCR0A=0;	TCCR0B |= (1<<CS02);	TCNT0 = 158;}void initadmux(){	ADMUX=0;	ADMUX |= (1<< REFS0) | (1<<ADLAR) | (1<<MUX1) | (1<<MUX0);//Referencia de 5v, Ajustado a la izq, Canal 3 de ADC	ADCSRA= 0;	ADCSRA |= (1<< ADPS1)|(1<< ADPS0)|(1<<ADATE) |(1<< ADIE)| (1<< ADEN);//Prescalers, Autotrigger, Interrupciónes habilitadas, ADC Enable.	ADCSRB =0; //Modo free running.}/**************************************/// Interrupt routinesISR(PCINT0_vect){		botonesPC = PINB;	TIFR0	|= (1 << TOV0);	TCNT0=158;	TIMSK0= (1<<TOIE0);}ISR(TIMER0_OVF_vect){	TIMSK0=0;	uint8_t botonest0 = PINB;	if (botonesPC == botonest0)	{		uint8_t temporal = botonest0 & (0b00000001);		if(temporal != 0b00000001)		{			contador++;		}		temporal = botonest0 & (0b00000010);		if(temporal != 0b00000010)		{			contador--;		}	}}ISR(ADC_vect){	if (unidades<=14)	{		unidades++;	}	else if (unidades>14)	{		unidades=0;		decenas++;	}}
