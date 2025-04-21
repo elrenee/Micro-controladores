58f1640a99f63e8720d3513de83af1339f885be4
elrenee <gon23365@uvg.edu.gt>
2025-04-20 19:51:24 -0600

    Comunicación serial
    Se configuro la comunicación serial para poder enviar un caracter a la terminal y para recibir un caracter de la terminal y mostrar su codigo ascii en el puerto B

---
+++ Laboratorio 06/Laboratorio 06/main.c
@@ -0,0 +1,8 @@
+ /*Lab06.c * * Created:19/04/2025  * Author: René David González Herrera * Description: Laboratorio 06, programación de microcontroladores. Comunicación serial. *//****************************************/// Encabezado (Libraries)#include <avr/io.h>#include <avr/interrupt.h>/****************************************/// Function prototypesvoid setup();void initUart();void escribirchar(char letra);/****************************************/// Main Function
+int main(void)
+{
+	setup();
+	escribirchar('3');
+	while (1)	{			}
+}
+/****************************************/// NON-Interrupt subroutinesvoid setup(){	cli();	initUart();	DDRB=0xFF;	PORTB=0x00;	sei();	}void initUart(){	//PInes de comunicación, PD0 y PD1  rx y tx	DDRD |= (1<<DDD1);	DDRD &= ~(1<<DDD0);			// Configuramos el UCSR0A, EN ESTE CASO NO LO UTILIZAREMOS	UCSR0A=0;	//Configuramos el UCSR0B, Habilitamos interrupciónes al recibir , Habilitamos recepcion y transmisión. 	UCSR0B |= (1<<RXCIE0)| (1<<RXEN0)| (1<<TXEN0);	// Modo asincrono.	UCSR0C =0;	UCSR0C |= (1<< UCSZ01) | (1<< UCSZ00); //Tamaño del caracter	//Nos da 9600 de baudrate con una frecuencia de reloj de 16Mhz	UBRR0= 103;}escribirchar(char letra){	while((UCSR0A & (1<<UDRE0))==0);//Vemos si el buffer No esta listo para recibir datos y lo hacemos esperar	 UDR0=letra; //Mandamos el caracter}/**************************************/// Interrupt routinesISR(USART_RX_vect){	char valor=UDR0;	PORTB= (uint8_t)valor;}
