0fec2e6a57eeb5dc5fef284a2e069a465dda5dc5
elrenee <gon23365@uvg.edu.gt>
2025-05-07 23:48:12 -0600

    Create PWM.h

---
+++ Proyecto/Proyecto/PWM1/PWM.h
@@ -0,0 +1,16 @@
+/*
+ * PWM.h
+ *
+ * Created: 28/04/2025 17:31:56
+ *  Author: Barbaritos
+ */ 
+
+
+#ifndef PWM_H_
+#define PWM_H_
+
+
+#include <avr/io.h>void initPWM();void initPWM2();void movservo1(uint16_t posicion);void movservo2(uint16_t servo2);void movservo3(uint16_t posicion3);void movservo4(uint16_t posicion4);
+
+
+#endif /* PWM_H_ */
