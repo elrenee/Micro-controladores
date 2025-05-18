9dd3f0fbd3218564d79f020dc8fe4cb98e1e60b9
elrenee <gon23365@uvg.edu.gt>
2025-05-18 14:22:09 -0600

    Desactivar lecturas del ADC
    Cuando no este en el modo 1, no quiero seguir actualizando el valor de los servos segun los potenciometros, asi que cuando no este en ese modo apagare las conversiones e interrupciónes.

---
+++ Proyecto_Servos/Proyecto_Servos/ADC/ADClib.c
@@ -0,0 +1,15 @@
+/*
+ * ADClib.c
+ *
+ * Created: 28/04/2025 16:49:55
+ *  Author: Barbaritos
+ */ 
+#include "ADClib.h"
+void initADC()
+{
+	ADMUX=0;	ADMUX |= (1<< REFS0) | (1<<ADLAR) | (1<<MUX2) | (1<<MUX1);//Referencia de 5v, Ajustado a la izq, Canal 6 de ADC	ADCSRA= 0;	ADCSRA |= (1<<ADPS2)| (1<< ADPS1)|(1<< ADPS0)|(1<< ADIE)| (1<< ADEN);//Prescalers, Interrupciónes habilitadas, ADC Enable.
+}
+void desactivar_ADC() 
+{
+	ADCSRA &= ~((1 << ADEN) | (1 << ADIE) | (1 << ADSC));
+}
---
+++ Proyecto_Servos/Proyecto_Servos/ADC/ADClib.h
@@ -0,0 +1,19 @@
+/*
+ * ADClib.h
+ *
+ * Created: 28/04/2025 16:50:11
+ *  Author: Barbaritos
+ */ 
+
+
+#ifndef ADCLIB_H_
+#define ADCLIB_H_
+
+
+#include <avr/io.h>
+void initADC();
+void desactivar_ADC();
+
+
+
+#endif /* ADCLIB_H_ */
